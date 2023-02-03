7cb480e8d33ec478672d46bdbefc4f3276bcde4e
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-02-03 17:23:52 +0300

    Replace href attribute with component Link, add code in comments

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Outlet, redirect } from 'react-router-dom';
+import { Outlet, Link /* , redirect, Navigate */ } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 
 import useAuth from '../hooks/index.jsx';
 
-export async function loader() {
-  const user = localStorage.getItem('user');
+/* export function loader() {
+  const user = localStorage.getItem('userInfo');
   if (!user) {
     throw redirect('/login');
   }
 
   return null;
-}
+} */
 
 function Root() {
   const auth = useAuth();
@@ -21,7 +21,7 @@ function Root() {
     <div className="d-flex flex-column h-100">
       <Navbar variant="light" bg="white" expand="lg" className="shadow-sm">
         <Container>
-          <Navbar.Brand href="/">Chat</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Chat</Navbar.Brand>
         </Container>
       </Navbar>
       <div id="main" className="h-100">
