34675bd1cf41e5fe0db3757a3a9f3bd118336835
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-02-05 01:15:59 +0300

    Start configurating RTK

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals.js';
 
+import store from './slices/index.js';
 import App from './App.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
