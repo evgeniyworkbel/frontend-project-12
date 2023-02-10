63e461e352abe8c5574da9101cfa56779cc52a5b
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-02-11 00:21:54 +0300

    Design message input

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { } from 'react-redux';
-import { } from 'react-bootstrap';
+import { Form, InputGroup, Button } from 'react-bootstrap';
+import { ArrowRightSquare } from 'react-bootstrap-icons';
+import { useFormik } from 'formik';
 
 function MessageInput() {
+  const formik = useFormik({
+    initialValues: {
+      newMessage: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <>
-    </>
+    <div className="mt-auto px-5 py-3">
+      <Form className="py-1 border rounded-2" disabled={formik.isSubmitting} onSubmit={formik.handleSubmit}>
+        <InputGroup>
+          <Form.Control
+            id="newMessage"
+            name="newMessage"
+            type="text"
+            className="p-0 ps-2 border-0"
+            placeholder="Введите сообщение..."
+            aria-label="Новое сообщение"
+            aria-describedby="submitBtn"
+            onChange={formik.handleChange}
+            value={formik.values.newMessage}
+          />
+          <Button type="submit" id="submitBtn" variant="" className="btn-group-vertical border-0" disabled={!formik.values.newMessage}>
+            <ArrowRightSquare size={20} />
+            <span className="visually-hidden">Отправить</span>
+          </Button>
+        </InputGroup>
+      </Form>
+    </div>
   );
 }
 
